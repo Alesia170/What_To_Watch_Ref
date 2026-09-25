93d2af75a0d8a1b629d85b0ad2cf666ff7b93cc0
Alesia <alesya.17039722@yandex.ru>
2026-09-25 09:09:05 +0300

    Refactor project structure

--- settings.py
+++ settings.py
@@ -1,11 +1,7 @@
 # settings.py
 import os
 
-from dotenv import load_dotenv
-
-load_dotenv
-
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
